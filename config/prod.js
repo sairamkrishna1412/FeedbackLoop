bb22617ad5249e595b839bddac7c8a72ddfeecf4
sai ram krishna <sairamkrishna1412@gmail.com>
2021-09-15 00:20:32 +0530

    Added Environment Variables (Dev and Prod)

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  facebookClientID: process.env.FACEBOOK_CLIENT_ID,
+  facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  mongoPass: process.env.MONGO_PASS,
+  cookieKey: process.env.COOKIE_KEY,
+};
