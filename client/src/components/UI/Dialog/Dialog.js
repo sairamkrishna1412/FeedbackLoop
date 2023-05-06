1cd1f547799d09f30976749f8e560b3f449074af
Krishna <sairamkrishna1412@gmail.com>
2023-05-06 21:55:49 +0530

    send more mails dialog bug fix

--- client/src/components/UI/Dialog/Dialog.js
+++ client/src/components/UI/Dialog/Dialog.js
@@ -10,7 +10,7 @@ export const Dialog = (props) => {
         onClick={props.onClose}
         className="fixed top-0 left-0 w-full h-full z-10 bg-black bg-opacity-40"
       ></div>
-      <PlainCard className="absolute z-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 m-0">
+      <PlainCard className="absolute z-20 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/3 m-0 md:-translate-y-1/2">
         <span
           className="absolute right-5 top-3 cursor-pointer text-4xl"
           onClick={props.onClose}
